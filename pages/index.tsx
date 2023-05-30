2f258b5f05bbdd7512cebce36968eec60df80cb1
Nidal468 <nidalstar1000@gmail.com>
2023-05-30 13:21:35 +0000

    byrrr

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ const MUBC: NextPage = () =>{
       </Head>
 			<img src="/images/bg.svg" className={styles.image}/>
 			<div className={styles.hero}>
-				<nav className="w-full h-[50px] flex items-start justify-between px-10"><div className="w-[300px] h-full flex items-center justify-between"><img src="/images/logo.ico" className="w-[40px]"/><h1 className="w-[240px] h-full flex items-center justify-center text-white">Monipur Uchcha Vidyalaya and College</h1></div><img src="/images/nav.svg"/><div className="w-[300px] h-full flex items-center justify-between text-white"><EmailIcon/><CallIcon/><YouTubeIcon/><TwitterIcon/><FacebookIcon/></div></nav>
+				<nav className="w-full h-[8vh] flex items-start justify-between px-[3vh]"><div className="w-[30vh] h-full flex items-center justify-between"><img src="/images/logo.ico" className="w-[4vh]"/><h1 className="w-[24vh] h-full flex items-center justify-center text-white text-[1.8vh]">Monipur Uchcha Vidyalaya and College</h1></div><img src="/images/nav.svg" className="w-[80vh]"/><div className="w-[30vh] h-full flex items-center justify-between text-white" id={styles.tags}><i className="fi fi-rr-envelope"></i><i className="fi fi-rr-phone-call"></i><i className="fi fi-brands-twitter"></i><i className="fi fi-brands-youtube"></i><i className="fi fi-brands-facebook"></i></div></nav>
 				<div className={styles.body}>
 					<div className={styles.right}>
 						<div className={styles.box} style={{display: 'none'}}>
