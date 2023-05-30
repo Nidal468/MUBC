5cbb510418d26060aba586650b23abc21238b94a
Nidal468 <nidalstar1000@gmail.com>
2023-05-30 08:36:56 +0000

    First commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import {Upload, Login, Logout} from '../components/main'
-import styles from '../styles/home.module.css'
+import styles from '../styles/mubc.module.css'
 import FacebookIcon from '@mui/icons-material/Facebook';
 import EmailIcon from '@mui/icons-material/Email';
 import YouTubeIcon from '@mui/icons-material/YouTube';
@@ -14,82 +14,31 @@ import "swiper/css/effect-fade";
 import { EffectFade, Navigation, Pagination, Autoplay } from "swiper";
 import { useTypewriter, Cursor } from 'react-simple-typewriter'
 
-const Home: NextPage = () => {
-	const [text] = useTypewriter({
-    words: ['Welcome to Monipur Uchcha Vidyalaya and College', 'Currently more then 40000 students study here', 'Please contact us if you want your children to study here'],
-    loop: 0
-  })
-  return (
-    <div className={styles.container}>
-      <Head>
+const MUBC: NextPage = () =>{
+	return(
+		<div className={styles.container}>
+		 <Head>
         <title>Monipur Uchcha Vidyalaya and College</title>
         <link rel="icon" href="/images/logo.ico" />
       </Head>
-			<main className={styles.body}>
-				<section className={styles.section1}>
-					<nav className={styles.nav}>
-						<div className={styles.navInfo}>
-							<div className='w-60 h-full flex items-center  justify-between'><img src='/images/logo.ico'/><h1 className='text-white w-40'>Monipur Uchcha Vidyalaya and College</h1></div>
-								<div className={styles.contact}>
-									<div className="w-[20vw] h-full flex items-center justify-between">
-									<EmailIcon fontSize="large"/>
-									<CallIcon fontSize="large"/>
-									<FacebookIcon fontSize="large"/>
-									<TwitterIcon fontSize="large"/>
-									<YouTubeIcon fontSize="large"/>
-									</div>
-									<div className="w-[18vw] h-full flex items-center justify-between"><Login/><Logout/>
-										<div className={styles.user}><h1>N</h1></div>
-									</div>
-									
-							</div>
-							
-						</div>
-						<div className={styles.links}>
-								<h4>HOME</h4>
-								<h4>ABOUT MUBC</h4>
-								<h4>ACADEMIC</h4>
-								<h4>ADMISSION</h4>
-								<h4>ADMINISTRATION</h4>
-								<h4>FACILITIES</h4>
-								<h4>COLLEGE CAMPUS</h4>
-								<h4>GALLERY</h4>
-								<h4>CONTACT</h4>
-						</div>
-					</nav>
-					<div className={styles.hero}>
-						<div className={styles.info}>
-							<h1>{text}<Cursor cursorColor="#fff"/></h1>
-							<p>In 2007, 2008, and 2009, the school was ranked third, and in 2010 and 2011 the school ranked sixth and seventh respectively and was ranked eighth in the 2012 SSC examination under the Dhaka Education Board. From 2011 to 2019 this school was ranked first in the PEC Examination.</p>
-							<div className="w-[25vw] h-[2vw] flex items-center justify-between"><div className={styles.button}>College Campus</div><div className={styles.button}>Academic</div></div>
-						</div>
-						<Swiper
-        spaceBetween={30}
-        effect={"fade"}
-        navigation={true}
-        pagination={{
-          clickable: true,
-        }}
-				autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-				loop={true}
-				centeredSlides={true}
-        modules={[EffectFade, Autoplay]}
-        className={styles.mySwiper}
-      ><SwiperSlide><img src="/images/teachers.jpg" /></SwiperSlide>
-				<SwiperSlide><img src="/images/monipur 2.jpg" /></SwiperSlide>
-				<SwiperSlide><img src="/images/monipur 3.jpg" /></SwiperSlide>
-						</Swiper>
+			<img src="/images/bg.svg" className={styles.image}/>
+			<div className={styles.hero}>
+				<nav className="w-full h-[50px] flex items-start justify-between px-10"><div className="w-[300px] h-full flex items-center justify-between"><img src="/images/logo.ico" className="w-[40px]"/><h1 className="w-[240px] h-full flex items-center justify-center text-white">Monipur Uchcha Vidyalaya and College</h1></div><img src="/images/nav.svg"/><div className="w-[300px] h-full flex items-center justify-between text-white"><EmailIcon/><CallIcon/><YouTubeIcon/><TwitterIcon/><FacebookIcon/></div></nav>
+				<div className={styles.body}>
+					<div className={styles.left}>
+						<img src="/images/image1.svg"/>
 					</div>
-				</section>
-				<section className={styles.section2}>
-					<div></div>
-				</section>
-			</main>
-    </div>
-  )
+					<div className={styles.right}>
+						<div className={styles.box}>
+							<h1 className="w-full h-[40px] flex items-center justify-start px-5 text-white">Latest News</h1>
+							<div className={styles.card}><h1>Bangla is still too hard in 2023</h1></div>
+						</div>
+					</div>
+				</div>
+				<img src="/images/bottombar.svg" style={{position: "absolute", zIndex: "999", bottom: "0"}}/>
+			</div>
+			
+		</div>
+	)
 }
-
-export default Home
+export default MUBC;
